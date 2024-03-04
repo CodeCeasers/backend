69f0d74c4b07d294e033e73a76ffc29452f6531d
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-05 00:03:55 +0530

    user routes

--- server/dist/routes/user.js
+++ server/dist/routes/user.js
@@ -14,6 +14,9 @@ const express = require('express');
 const zod = require('zod');
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET = process.env.JSON_WEB_TOKEN_SECRET;
 exports.userRoute = express.Router();
 exports.userRoute.get('/', (req, res) => {
     res.json({
@@ -21,9 +24,9 @@ exports.userRoute.get('/', (req, res) => {
     });
 });
 const userZodVerify = zod.object({
-    email: zod.string(),
+    email: zod.string().min(3),
     password: zod.string().min(3),
-    name: zod.string().optional(),
+    name: zod.string(),
     role: zod.string()
 });
 exports.userRoute.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -60,11 +63,11 @@ exports.userRoute.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0
             name: newUser.name,
             role: newUser.role
         };
-        // generate token
-        // token = jwt.sign(userDetails, process.env.JWT_SECRET)
+        const token = jwt.sign(userDetails, JWT_SECRET);
         res.status(200).json({
             msg: "User Created",
-            user: userDetails
+            user: userDetails,
+            token: token
         });
     }
     catch (err) {
@@ -73,4 +76,43 @@ exports.userRoute.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0
         });
         return;
     }
+}));
+const userSigninZodVerify = zod.object({
+    email: zod.string(),
+    password: zod.string().min(3)
+});
+exports.userRoute.post('/signin', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(req.body);
+    const verifyZod = userSigninZodVerify.safeParse(req.body);
+    if (!verifyZod.success) {
+        res.status(404).json({
+            msg: "Zod cannot verify the data"
+        });
+        return;
+    }
+    try {
+        const findUser = yield prisma.user.findFirst({
+            where: {
+                email: req.body.email
+            }
+        });
+        if (findUser != null) {
+            const token = jwt.sign({ email: findUser.email, name: findUser.name, role: findUser.role }, JWT_SECRET);
+            res.status(200).json({
+                msg: "User Signin",
+                user: findUser,
+                token: token
+            });
+            return;
+        }
+    }
+    catch (err) {
+        res.status(411).json({
+            msg: err.message
+        });
+        return;
+    }
+    res.status(411).json({
+        msg: "User does not exist"
+    });
 }));
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET = process.env.JSON_WEB_TOKEN_SECRET;
+
 export const userRoute = express.Router();
 
 userRoute.get('/', (req:any, res:any) => {
@@ -13,9 +17,9 @@ userRoute.get('/', (req:any, res:any) => {
 });
 
 const userZodVerify = zod.object({
-    email: zod.string(),
+    email: zod.string().min(3),
     password: zod.string().min(3),
-    name:zod.string().optional(),
+    name:zod.string(),
     role: zod.string()
 })
 
@@ -55,11 +59,11 @@ userRoute.post('/signup', async (req:any, res:any)=> {
             role: newUser.role
         }
         
-        // generate token
-        // token = jwt.sign(userDetails, process.env.JWT_SECRET)
+        const token = jwt.sign(userDetails, JWT_SECRET)
         res.status(200).json({
             msg: "User Created",
-            user: userDetails
+            user: userDetails,
+            token: token
         })
     } catch(err:any){
         res.status(411).json({
@@ -68,3 +72,44 @@ userRoute.post('/signup', async (req:any, res:any)=> {
         return;
     }
 })
+
+const userSigninZodVerify = zod.object({
+    email: zod.string(),
+    password: zod.string().min(3)
+})
+
+
+userRoute.post('/signin', async (req:any, res:any)=> {
+    console.log(req.body);
+    const verifyZod = userSigninZodVerify.safeParse(req.body)
+    if(!verifyZod.success){
+        res.status(404).json({
+            msg:"Zod cannot verify the data"
+        })
+        return;
+    }
+    try{
+        const findUser = await prisma.user.findFirst({
+            where: {
+                email: req.body.email
+            }
+        })
+        if(findUser != null){
+            const token = jwt.sign({email: findUser.email, name: findUser.name, role: findUser.role}, JWT_SECRET);
+            res.status(200).json({
+                msg: "User Signin",
+                user: findUser,
+                token: token
+            })
+            return;
+        }
+    } catch(err:any){
+        res.status(411).json({
+            msg:err.message
+        })
+        return;
+    }
+    res.status(411).json({
+        msg: "User does not exist"
+    })
+});
